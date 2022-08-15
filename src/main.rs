ee7ba6d6cbc76696cdb5b6435b558b09270206b4
Rex Zhang <rexzhang4321@gmail.com>
2022-08-14 22:30:17 -0700

    render final scene

--- src/main.rs
+++ src/main.rs
@@ -51,32 +51,18 @@ fn ray_color(r: &Ray, hittable: &impl Hittable, depth: i32) -> Color {
 fn main() -> std::io::Result<()> {
     
     // image
-    let aspect_ratio: f32 = 16.0 / 9.0;
-    let image_width: i32 = 400;
+    let aspect_ratio: f32 = 3.0 / 2.0;
+    let image_width: i32 = 1200;
     let image_height: i32 = (image_width as f32 / aspect_ratio) as i32;
     let samples_per_pixel: i32 = 100;
     let max_depth: i32 = 50;
 
     // world
-    let mut world = HittableList { objects: Vec::new() };
-
-    let material_ground = Rc::new(Lambertian {albedo: Color::new(0.8, 0.8, 0.0)});
-    let material_center = Rc::new(Lambertian {albedo: Color::new(0.1, 0.2, 0.5)});
-    // let material_left = Rc::new(Metal {albedo: Color::new(0.8, 0.8, 0.8), fuzz: 0.3});
-    //let material_center = Rc::new(Dielectric {ir: 1.5});
-    let material_left = Rc::new(Dielectric {ir: 1.5});
-    let material_right = Rc::new(Metal {albedo: Color::new(0.8, 0.6, 0.2), fuzz: 0.0});
-
-    world.add(Rc::new(Sphere { center: Point3 {x: 0.0, y: -100.5, z: -1.0}, radius: 100.0, material: material_ground }));
-    world.add(Rc::new(Sphere { center: Point3 {x: 0.0, y: 0.0, z: -1.0}, radius: 0.5, material: material_center}));
-    world.add(Rc::new(Sphere { center: Point3 {x: -1.0, y: 0.0, z: -1.0}, radius: 0.5, material: Rc::clone(&material_left) as Rc<dyn Material>}));
-    world.add(Rc::new(Sphere { center: Point3 {x: -1.0, y: 0.0, z: -1.0}, radius: -0.4, material: Rc::clone(&material_left) as Rc<dyn Material>}));
-    world.add(Rc::new(Sphere { center: Point3 {x: 1.0, y: 0.0, z: -1.0}, radius: 0.5, material: material_right}));
-
+    let world = random_scene();
 
     // camera
-    let look_from = Point3::new(3.0, 3.0, 2.0);
-    let look_to = Point3::new(0.0, 0.0, -1.0);
+    let look_from = Point3::new(13.0, 2.0, 3.0);
+    let look_to = Point3::new(0.0, 0.0, 0.0);
 
     let camera = Camera::new(
         look_from,
@@ -84,8 +70,8 @@ fn main() -> std::io::Result<()> {
         Vec3::new(0.0, 1.0, 0.0),
         20.0,
         aspect_ratio,
-        1.0,
-        (look_from - look_to).length()
+        0.1,
+        10.0
     );
 
     // render
@@ -113,4 +99,50 @@ fn main() -> std::io::Result<()> {
     output.write(buffer.as_bytes())?;
 
     Ok(())
+}
+
+fn random_scene() -> HittableList {
+    let mut world = HittableList { objects: Vec::new() };
+
+    let ground_material = Rc::new(Lambertian {albedo: Color::new(0.5, 0.5, 0.5)});
+    world.add(Rc::new(Sphere { center: Point3 {x: 0.0, y: -1000.0, z: 0.0}, radius: 1000.0, material: ground_material }));
+
+    let mut rng = rand::thread_rng();
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rng.gen::<f32>();
+            let center = Point3::new(a as f32 + 0.9 * rng.gen::<f32>(), 0.2, b as f32 + 0.9 * rng.gen::<f32>());
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                let sphere_material: Rc<dyn Material>;
+
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Color::random() * Color::random();
+                    sphere_material = Rc::new(Lambertian {albedo: albedo});
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz: f32 = rng.gen_range(0.0..0.5);
+                    sphere_material = Rc::new(Metal {albedo, fuzz});
+                } else {
+                    // glass
+                    sphere_material = Rc::new(Dielectric {ir: 1.5});
+                }
+                world.add(Rc::new(Sphere {center: center, radius: 0.2, material: sphere_material}));
+            }
+        }
+    }
+
+    let material_1 = Rc::new(Dielectric {ir: 1.5});
+    world.add(Rc::new(Sphere {center: Point3::new(0.0, 1.0, 0.0), radius: 1.0, material: material_1}));
+
+    
+    let material_2 = Rc::new(Lambertian {albedo: Color::new(0.4, 0.2, 0.1)});
+    world.add(Rc::new(Sphere {center: Point3::new(-4.0, 1.0, 0.0), radius: 1.0, material: material_2}));
+
+    
+    let material_3 = Rc::new(Metal {albedo: Color::new(0.7, 0.6, 0.5), fuzz: 0.0});
+    world.add(Rc::new(Sphere {center: Point3::new(4.0, 1.0, 0.0), radius: 1.0, material: material_3}));
+    world
 }
